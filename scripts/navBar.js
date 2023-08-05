d0e8a38648d8ddb2e82432aacadc5f006fff78d3
Jadnell <jadnellreyes@gmail.com>
2023-08-05 00:32:21 -0400

    Fixed navBar unidentified parameter

--- scripts/navBar.js
+++ scripts/navBar.js
@@ -1,6 +1,7 @@
 // Script for Navigation Bar Operation Onclick
 var selectMenu = document.getElementById("selectMenu");
 var menuButtonsList = document.getElementById("menuButtonsList");
+var options = document.getElementsByClassName("options");
 
 selectMenu.onclick = function () {
   menuButtonsList.classList.toggle("hide");
