3fdf0b081b4501aee809194fd7269cd8b751df26
Jadnell <jadnellreyes@gmail.com>
2023-08-17 20:38:58 -0400

    Added same functionality to nav menu

--- scripts/navBar.js
+++ scripts/navBar.js
@@ -3,12 +3,24 @@ var selectMenu = document.getElementById("selectMenu");
 var menuButtonsList = document.getElementById("menuButtonsList");
 var options = document.getElementsByClassName("options");
 
-selectMenu.onclick = function () {
+selectMenu.onclick = function (event) {
+  event.stopPropagation();
   menuButtonsList.classList.toggle("hide");
-}; // end function
+};
 
 for (Option of options) {
-  Option.onclick = function () {
+  Option.onclick = function (event) {
+    event.stopPropagation();
     menuButtonsList.classList.toggle("hide");
-  }; // end function
-} // end for
+  };
+}
+
+// Add click event listener to the document
+document.addEventListener("click", function (event) {
+  if (
+    !selectMenu.contains(event.target) &&
+    !menuButtonsList.contains(event.target)
+  ) {
+    menuButtonsList.classList.add("hide");
+  }
+});
