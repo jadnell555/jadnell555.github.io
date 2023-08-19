0e14494e6386ea75eb7e354e03c400b3c1b38773
Jadnell <jadnellreyes@gmail.com>
2023-08-18 21:04:13 -0400

    Testing of contactForm name validation

--- scripts/contactForm.js
+++ scripts/contactForm.js
@@ -1,3 +1,29 @@
-function validateEmail(email) {}
+function validateEmail() {}
 
-function validateNames() {}
+function validateName() {
+  const nameRegex = /^[A-Za-z\u00C0-\u017F']+(\s[A-Za-z\u00C0-\u017F']{1,2})?$/;
+  const minLength = 3;
+  const maxLength = 25;
+  var name = document.getElementById("firstName").value;
+  var trimmedName = trim(name);
+
+  if (trimmedName.length < minLength || trimmedName.length > maxLength) {
+    alert(
+      "Name too short or too long. Names should be between" +
+        minLength +
+        "-" +
+        maxLength +
+        "characters."
+    );
+  } // end if
+
+  if (!nameRegex.test(trimmedName)) {
+    alert(
+      "Invalid name entered. Please enter your name without numbers or special characters"
+    );
+  } // end if
+
+  return true; // Validation passed
+} // end validateName()
+
+function validateLastName() {}
