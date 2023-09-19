6f7f1821187bcf1fb7497ece7aaa21e5943f9415
Jadnell <jadnellreyes@gmail.com>
2023-09-18 21:54:19 -0400

    Added function to handle different screen sizes to techStack

--- scripts/techStackButtons.js
+++ scripts/techStackButtons.js
@@ -49,6 +49,73 @@ setupToggle("JupyterButton", "JupyterInfo");
 setupToggle("FlutterButton", "FlutterInfo");
 setupToggle("DjangoButton", "DjangoInfo");
 */
+const buttons = [
+  "HTMLButton",
+  "CSSButton",
+  "JSButton",
+  "PythonButton",
+  "CPPButton",
+  "DartButton",
+  "SQLButton",
+  "MATLABButton",
+  "VSCODEButton",
+  "AndroidStudioButton",
+  "VSButton",
+  "VirtualBoxButton",
+  "RaspberryPiButton",
+  "ArduinoButton",
+  "GitHubButton",
+  "JupyterButton",
+  "FlutterButton",
+  "DjangoButton",
+];
+const containers = [
+  "HTMLContainer",
+  "CSSContainer",
+  "JSContainer",
+  "PythonContainer",
+  "CPPContainer",
+  "DartContainer",
+  "SQLContainer",
+  "MATLABContainer",
+  "VSCODEContainer",
+  "AndroidStudioContainer",
+  "VSContainer",
+  "VirtualBoxContainer",
+  "RaspberryPiContainer",
+  "ArduinoContainer",
+  "GitHubContainer",
+  "JupyterContainer",
+  "FlutterContainer",
+  "DjangoContainer",
+];
+const arrows = [
+  "HTMLArrow",
+  "CSSArrow",
+  "JSArrow",
+  "PythonArrow",
+  "CPPArrow",
+  "DartArrow",
+  "SQLArrow",
+  "MATLABArrow",
+  "VSCODEArrow",
+  "AndroidStudioArrow",
+  "VSArrow",
+  "VirtualBoxArrow",
+  "RaspberryPiArrow",
+  "ArduinoArrow",
+  "GitHubArrow",
+  "JupyterArrow",
+  "FlutterArrow",
+  "DjangoArrow",
+];
+const sizes = [
+  10, 9, 12, 13, 9, 13, 15, 10, 15, 11, 13, 9, 13, 13, 9, 15, 10, 14,
+];
+const sizesDesktop = [
+  9, 9, 10, 10, 9, 10, 11, 9, 11, 9, 10, 9, 10, 10, 9, 11, 9, 11,
+];
+
 function expandableButton(techButton, container, arrow, expandedHeight) {
   var button = document.getElementById(techButton);
   var techStackContainer = document.getElementById(container);
@@ -68,6 +135,24 @@ function expandableButton(techButton, container, arrow, expandedHeight) {
   });
 }
 
+let media = window.matchMedia("(min-width: 900px");
+
+function screenSize() {
+  if (media.matches) {
+    for (i = 0; i < buttons.length; i++) {
+      expandableButton(buttons[i], containers[i], arrows[i], sizesDesktop[i]);
+    }
+  } else {
+    for (i = 0; i < buttons.length; i++) {
+      expandableButton(buttons[i], containers[i], arrows[i], sizes[i]);
+    }
+  }
+}
+
+media.addEventListener("change", screenSize());
+screenSize(media);
+
+/*
 expandableButton("HTMLButton", "HTMLContainer", "HTMLArrow", 10);
 expandableButton("CSSButton", "CSSContainer", "CSSArrow", 9);
 expandableButton("JSButton", "JSContainer", "JSArrow", 12);
@@ -101,3 +186,4 @@ expandableButton("GitHubButton", "GitHubContainer", "GitHubArrow", 9);
 expandableButton("JupyterButton", "JupyterContainer", "JupyterArrow", 15);
 expandableButton("FlutterButton", "FlutterContainer", "FlutterArrow", 10);
 expandableButton("DjangoButton", "DjangoContainer", "DjangoArrow", 14);
+*/
