f5d5ca3d176d2d3aef2252c7eaeca05c25edcc9c
Jadnell <jadnellreyes@gmail.com>
2024-02-23 05:10:36 -0400

    Major design change. Project section redesigned, simplified index.html & lightDesign.css, new color palette.

---
+++ scripts/imageSlideshowProj5.js
@@ -0,0 +1,36 @@
+let slideIndex = 1;
+let slideId = "mySlides5 fade"; // Variable to identify how many images in slideshow
+let dot = "dot5";
+
+showSlides(1);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName(slideId);
+  let dots = document.getElementsByClassName(dot);
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
